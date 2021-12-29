3f30ea78c3869f2a5a919a1b5dd4b2a3a425dc9c
PantherDave <dacos062@fiu.edu>
2021-12-29 14:11:09 -0500

    Rectified test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Vladimir Putin";
+            return "David Acosta";
         }
 
         // POST api/values
