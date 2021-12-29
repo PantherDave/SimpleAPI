96fabdc0444a49a1e4fe5f58957df86d87c37cd8
PantherDave <dacos062@fiu.edu>
2021-12-29 12:20:41 -0500

    Changed test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "David Acosta";
+            return "Vladimir Putin";
         }
 
         // POST api/values
